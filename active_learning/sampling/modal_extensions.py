0b19117d44f5b4469f0e97db5cfdba916f8265c3
Matthias Pfenninger <matthias.pfenninger@gmail.com>
2021-05-03 15:12:55 +0200

    Format files via black, update dependencies

--- active_learning/sampling/modal_extensions.py
+++ active_learning/sampling/modal_extensions.py
@@ -20,7 +20,10 @@ OUTLIER_FRACTION = 0.2
 EMBEDDING_LAYER = "avgpool"
 EMBEDDING_SIZE = 2048
 
-def max_entropy(learner: ActiveLearner, X: np.array, n_instances: int = N_INSTANCES_DEFAULT, T: int = T_DEFAULT) -> Tuple[torch.Tensor, np.array]:
+
+def max_entropy(
+    learner: ActiveLearner, X: np.array, n_instances: int = N_INSTANCES_DEFAULT, T: int = T_DEFAULT
+) -> Tuple[torch.Tensor, np.array]:
     """Active learning sampling technique that maximizes the predictive entropy based on the paper
     'Deep Bayesian Active Learning with Image Data' (https://arxiv.org/pdf/1703.02910.pdf).
 
@@ -36,7 +39,7 @@ def max_entropy(learner: ActiveLearner, X: np.array, n_instances: int = N_INSTAN
     learner : modAL.models.ActiveLearner
       modAL ActiveLearner instance with which the sampling technique should be used
 
-    X : numpy.array 
+    X : numpy.array
       Array of instances from which to sample from
 
     n_instances : int (default = 100)
@@ -53,10 +56,12 @@ def max_entropy(learner: ActiveLearner, X: np.array, n_instances: int = N_INSTAN
     return _batched_pytorch_tensor_loop(learner, X, n_instances, _max_entropy_scoring_function, T=T)
 
 
-def bald(learner: ActiveLearner, X: np.array, n_instances: int = N_INSTANCES_DEFAULT, T: int = T_DEFAULT) -> Tuple[torch.Tensor, np.array]:
-    """Active learning sampling technique that maximizes the information gain via maximising mutual information between predictions 
-    and model posterior (Bayesian Active Learning by Disagreement - BALD) as depicted in the papers 'Deep Bayesian Active Learning 
-    with Image Data' (https://arxiv.org/pdf/1703.02910.pdf) and 'Bayesian Active Learning for Classification and Preference Learning' 
+def bald(
+    learner: ActiveLearner, X: np.array, n_instances: int = N_INSTANCES_DEFAULT, T: int = T_DEFAULT
+) -> Tuple[torch.Tensor, np.array]:
+    """Active learning sampling technique that maximizes the information gain via maximising mutual information between predictions
+    and model posterior (Bayesian Active Learning by Disagreement - BALD) as depicted in the papers 'Deep Bayesian Active Learning
+    with Image Data' (https://arxiv.org/pdf/1703.02910.pdf) and 'Bayesian Active Learning for Classification and Preference Learning'
     (https://arxiv.org/pdf/1112.5745.pdf).
 
     Examples
@@ -71,7 +76,7 @@ def bald(learner: ActiveLearner, X: np.array, n_instances: int = N_INSTANCES_DEF
     learner : modAL.models.ActiveLearner
       modAL ActiveLearner instance with which the sampling technique should be used
 
-    X : numpy.array 
+    X : numpy.array
       Array of instances from which to sample from
 
     n_instances : int (default = 100)
@@ -88,7 +93,9 @@ def bald(learner: ActiveLearner, X: np.array, n_instances: int = N_INSTANCES_DEF
     return _batched_pytorch_tensor_loop(learner, X, n_instances, _bald_scoring_function, T=T)
 
 
-def random(learner: ActiveLearner, X: np.array, n_instances: int = N_INSTANCES_DEFAULT) -> Tuple[torch.Tensor, np.array]:
+def random(
+    learner: ActiveLearner, X: np.array, n_instances: int = N_INSTANCES_DEFAULT
+) -> Tuple[torch.Tensor, np.array]:
     """Baseline active learning sampling technique that takes random instances from available pool.
 
     Examples
@@ -103,7 +110,7 @@ def random(learner: ActiveLearner, X: np.array, n_instances: int = N_INSTANCES_D
     learner : modAL.models.ActiveLearner
       modAL ActiveLearner instance with which the sampling technique should be used
 
-    X : numpy.array 
+    X : numpy.array
       Array of instances from which to sample from
 
     n_instances : int (default = 100)
@@ -113,17 +120,17 @@ def random(learner: ActiveLearner, X: np.array, n_instances: int = N_INSTANCES_D
     -------
     Tuple of indexes and corresponding data instances that were chosen based on the sampling strategy.
     """
-    
+
     query_idx = np.random.choice(range(len(X)), size=n_instances, replace=False)
     return query_idx, X[query_idx]
 
 
-def _batched_pytorch_tensor_loop(learner, X, n_instances, batch_scoring_function, **kwargs): 
+def _batched_pytorch_tensor_loop(learner, X, n_instances, batch_scoring_function, **kwargs):
 
     if DEBUG_OUTPUT:
         print("Processing pool of instances with selected active learning strategy...")
         print("(Note: Based on the pool size this takes a while. Will generate debug output every 10%.)")
-        ten_percent = int(len(X)/BATCH_SIZE/10)
+        ten_percent = int(len(X) / BATCH_SIZE / 10)
         i = 0
         percentage_output = 10
 
@@ -137,7 +144,7 @@ def _batched_pytorch_tensor_loop(learner, X, n_instances, batch_scoring_function
     for batch in all_samples:
 
         acquisitions = batch_scoring_function(learner, batch, **kwargs)
-        
+
         all_acquisitions = torch.cat([all_acquisitions, acquisitions])
 
         if DEBUG_OUTPUT:
@@ -156,16 +163,19 @@ def _bald_scoring_function(learner, batch, T):
 
     with torch.no_grad():
 
-        outputs = torch.stack([
-            torch.softmax( # probabilities from logits
-                learner.estimator.forward(batch, training=True, device=DEVICE), dim=-1) # logits
-            for t in range(T) # multiple calculations to average over
-            ])
+        outputs = torch.stack(
+            [
+                torch.softmax(  # probabilities from logits
+                    learner.estimator.forward(batch, training=True, device=DEVICE), dim=-1
+                )  # logits
+                for t in range(T)  # multiple calculations to average over
+            ]
+        )
 
     mean_outputs = torch.mean(outputs, dim=0)
 
-    H = torch.sum(-mean_outputs*torch.log(mean_outputs + 1e-10), dim=-1)
-    E_H = - torch.mean(torch.sum(outputs * torch.log(outputs + 1e-10), dim=-1), dim=0)
+    H = torch.sum(-mean_outputs * torch.log(mean_outputs + 1e-10), dim=-1)
+    E_H = -torch.mean(torch.sum(outputs * torch.log(outputs + 1e-10), dim=-1), dim=0)
     acquisitions = H - E_H
 
     return acquisitions
@@ -175,11 +185,14 @@ def _max_entropy_scoring_function(learner, batch, T):
 
     with torch.no_grad():
 
-        outputs = torch.stack([
-            torch.softmax( # probabilities from logits
-                learner.estimator.forward(batch, training=True, device=DEVICE), dim=-1) # logits
-            for t in range(T) # multiple calculations to average over
-            ])
+        outputs = torch.stack(
+            [
+                torch.softmax(  # probabilities from logits
+                    learner.estimator.forward(batch, training=True, device=DEVICE), dim=-1
+                )  # logits
+                for t in range(T)  # multiple calculations to average over
+            ]
+        )
 
     mean_outputs = torch.mean(outputs, dim=0)
 
@@ -191,7 +204,7 @@ def _featurize_instances(learner, X):
     if DEBUG_OUTPUT:
         print("Featurizing pool of instances for diversity sampling...")
         print("(Note: Based on the pool size this takes a while. Will generate debug output every 10%.)")
-        ten_percent = int(len(X)/BATCH_SIZE/10)
+        ten_percent = int(len(X) / BATCH_SIZE / 10)
         i = 0
         percentage_output = 10
 
@@ -208,7 +221,7 @@ def _featurize_instances(learner, X):
     def copy_data(m, i, o):
         batch_features.copy_(o.data)
 
-    hook = layer.register_forward_hook(copy_data)     
+    hook = layer.register_forward_hook(copy_data)
 
     # process pool of instances batch wise to featurize
     for batch in all_samples:
@@ -216,7 +229,7 @@ def _featurize_instances(learner, X):
         # if last batch smaller: avoid size mismatch
         if batch.shape[0] < BATCH_SIZE:
             batch_features = torch.zeros(batch.shape[0], EMBEDDING_SIZE, 1, 1)
-        
+
         # feed batch into network (get out features via registered hook)
         _ = learner.estimator.forward(batch, training=False, device=DEVICE)
 
@@ -225,7 +238,7 @@ def _featurize_instances(learner, X):
         if DEBUG_OUTPUT:
             i += 1
             if i > 2:
-                break 
+                break
             if i > ten_percent:
                 print(f"{percentage_output}% of samples in pool featurized")
                 percentage_output += 10
@@ -246,16 +259,23 @@ def _cluster_instances(all_features, min_cluster_size):
     n_clusters = -1
     while n_clusters < 1:
         clusterer = hdbscan.HDBSCAN(min_cluster_size=min_cluster_size).fit(all_features)
-        n_clusters = clusterer.labels_.max()+1
+        n_clusters = clusterer.labels_.max() + 1
         if n_clusters < 1:
             if DEBUG_OUTPUT:
-                print(f"HDBSCAN was not able to find clusters, halving min. cluster size from {min_cluster_size} to {int(min_cluster_size/2)}")
-            min_cluster_size = int(min_cluster_size/2)
+                print(
+                    f"HDBSCAN was not able to find clusters, halving min. cluster size from {min_cluster_size} to {int(min_cluster_size/2)}"
+                )
+            min_cluster_size = int(min_cluster_size / 2)
 
     return clusterer, n_clusters
 
 
-def outlier(learner: ActiveLearner, X: np.array, n_instances: int = N_INSTANCES_DEFAULT, min_cluster_size: int = HDBSCAN_MIN_CLUSTER_SIZE) -> Tuple[torch.Tensor, np.array]:
+def outlier(
+    learner: ActiveLearner,
+    X: np.array,
+    n_instances: int = N_INSTANCES_DEFAULT,
+    min_cluster_size: int = HDBSCAN_MIN_CLUSTER_SIZE,
+) -> Tuple[torch.Tensor, np.array]:
     """Active learning sampling technique that translates instances from the pool to features by using an internal layer of the
     learner model as featurizer, performing HDBSCAN to cluster the instances and then returning the outliers based on their
     GLOSH score.
@@ -272,7 +292,7 @@ def outlier(learner: ActiveLearner, X: np.array, n_instances: int = N_INSTANCES_
     learner : modAL.models.ActiveLearner
       modAL ActiveLearner instance with which the sampling technique should be used
 
-    X : numpy.array 
+    X : numpy.array
       Array of instances from which to sample from
 
     n_instances : int (default = 100)
@@ -296,9 +316,14 @@ def outlier(learner: ActiveLearner, X: np.array, n_instances: int = N_INSTANCES_
     return idx, X[idx]
 
 
-def cluster_outlier_combined(learner: ActiveLearner, X: np.array, n_instances: int = N_INSTANCES_DEFAULT, min_cluster_size: int = HDBSCAN_MIN_CLUSTER_SIZE) -> Tuple[torch.Tensor, np.array]:
+def cluster_outlier_combined(
+    learner: ActiveLearner,
+    X: np.array,
+    n_instances: int = N_INSTANCES_DEFAULT,
+    min_cluster_size: int = HDBSCAN_MIN_CLUSTER_SIZE,
+) -> Tuple[torch.Tensor, np.array]:
     """Active learning sampling technique that translates instances from the pool to features by using an internal layer of the
-    learner model as featurizer, performing HDBSCAN to cluster the instances and then returning a combination of instances spread 
+    learner model as featurizer, performing HDBSCAN to cluster the instances and then returning a combination of instances spread
     across all clusters and outliers detected via GLOSH.
 
     Examples
@@ -313,7 +338,7 @@ def cluster_outlier_combined(learner: ActiveLearner, X: np.array, n_instances: i
     learner : modAL.models.ActiveLearner
       modAL ActiveLearner instance with which the sampling technique should be used
 
-    X : numpy.array 
+    X : numpy.array
       Array of instances from which to sample from
 
     n_instances : int (default = 100)
@@ -335,18 +360,18 @@ def cluster_outlier_combined(learner: ActiveLearner, X: np.array, n_instances: i
     # initialize variables needed below
     outlier_threshold = pd.Series(clusterer.outlier_scores_).quantile(OUTLIER_QUANTILE)
     n_features = len(all_features)
-    instances_per_cluster = int(n_instances/n_clusters)
+    instances_per_cluster = int(n_instances / n_clusters)
     all_selected_idx = np.array([]).astype(int)
 
     # check whether distribution between clusters can be done even - if not, distribute remaining equally
     if n_instances % n_clusters != 0:
-        leftover_instances = n_instances-instances_per_cluster*n_clusters
+        leftover_instances = n_instances - instances_per_cluster * n_clusters
     else:
         leftover_instances = 0
 
     if DEBUG_OUTPUT:
         print(f"{n_clusters} clusters found, entering loop to pick instances in all of them...")
-    
+
     # loop over clusters to select instances in all of them
     for cluster_id in range(n_clusters):
 
@@ -359,8 +384,8 @@ def cluster_outlier_combined(learner: ActiveLearner, X: np.array, n_instances: i
         else:
             instances_in_cluster = instances_per_cluster
 
-        n_outliers = int(instances_in_cluster*OUTLIER_FRACTION)
-        n_regular = instances_in_cluster-n_outliers
+        n_outliers = int(instances_in_cluster * OUTLIER_FRACTION)
+        n_regular = instances_in_cluster - n_outliers
 
         # limit pool to current cluster only
         cluster_idx_all = np.where(clusterer.labels_ == cluster_id)[0]
@@ -369,24 +394,28 @@ def cluster_outlier_combined(learner: ActiveLearner, X: np.array, n_instances: i
 
         # avoid having a pool that is too small to sample from
         if len(regular_idx_all) < n_regular:
-            leftover_instances += (n_regular-len(regular_idx_all))
+            leftover_instances += n_regular - len(regular_idx_all)
             n_regular = len(regular_idx_all)
         if len(outlier_idx_all) < n_outliers:
-            leftover_instances += (n_outliers-len(outlier_idx_all))
+            leftover_instances += n_outliers - len(outlier_idx_all)
             n_outliers = len(outlier_idx_all)
 
         # select both regular and outlier instances for this cluster
-        regular_idx_selected = np.random.choice(len(regular_idx_all), size=n_regular, replace=False)      
+        regular_idx_selected = np.random.choice(len(regular_idx_all), size=n_regular, replace=False)
         outlier_idx_selected = np.random.choice(len(outlier_idx_all), size=n_outliers, replace=False)
 
         # calculate all idx that are selected from this cluster
         if n_outliers > 0:
-            outlier_idx_original = np.array(range(n_features))[cluster_idx_all][outlier_idx_all][outlier_idx_selected] # outlier idx of this cluster (idx translated to full pool)
+            outlier_idx_original = np.array(range(n_features))[cluster_idx_all][outlier_idx_all][
+                outlier_idx_selected
+            ]  # outlier idx of this cluster (idx translated to full pool)
         else:
             outlier_idx_original = np.array([]).astype(int)
 
         if n_regular > 0:
-            regular_idx_original = np.array(range(n_features))[cluster_idx_all][regular_idx_all][regular_idx_selected] # regular idx of this cluster (idx translated to full pool)
+            regular_idx_original = np.array(range(n_features))[cluster_idx_all][regular_idx_all][
+                regular_idx_selected
+            ]  # regular idx of this cluster (idx translated to full pool)
         else:
             regular_idx_original = np.array([]).astype(int)
 
@@ -398,6 +427,8 @@ def cluster_outlier_combined(learner: ActiveLearner, X: np.array, n_instances: i
             print(f"Instance selection on cluster no. {cluster_id} done.")
 
     if len(all_selected_idx) < n_instances:
-        print(f"CAUTION: cluster_outlier_combined algorithm was not able to build a set of {n_instances} instances as requested, instead only returning {len(all_selected_idx)} instances.")
+        print(
+            f"CAUTION: cluster_outlier_combined algorithm was not able to build a set of {n_instances} instances as requested, instead only returning {len(all_selected_idx)} instances."
+        )
 
     return all_selected_idx, X[all_selected_idx]
--- training/run_modal_experiment.py
+++ training/run_modal_experiment.py
@@ -18,12 +18,14 @@ from active_learning import lit_models
 np.random.seed(42)
 torch.manual_seed(42)
 
+
 def _import_class(module_and_class_name: str) -> type:
     """Import class from a module, e.g. 'active_learning.models.MLP'"""
     module_name, class_name = module_and_class_name.rsplit(".", 1)
     module = importlib.import_module(module_name)
     class_ = getattr(module, class_name)
     return class_
+
 
 def _setup_parser():
     """Set up Python's ArgumentParser with data, model, trainer, and other arguments."""
@@ -56,37 +58,81 @@ def _setup_parser():
     lit_models.BaseLitModel.add_to_argparse(lit_model_group)
 
     # Active learning specific arguments
-    parser.add_argument("--al_epochs_init", type=int, default=20, help="No. of initial epochs to train before beginning with active learning")
-    parser.add_argument("--al_epochs_incr", type=int, default=20, help="No. of epochs to train in each active learning iteration")
+    parser.add_argument(
+        "--al_epochs_init",
+        type=int,
+        default=20,
+        help="No. of initial epochs to train before beginning with active learning",
+    )
+    parser.add_argument(
+        "--al_epochs_incr", type=int, default=20, help="No. of epochs to train in each active learning iteration"
+    )
     parser.add_argument("--al_n_iter", type=int, default=33, help="No. of active learning iterations")
-    parser.add_argument("--al_samples_per_iter", type=int, default=2000, help="No. of samples to query per active learning iteration")
-    parser.add_argument("--al_incr_onlynew", type=bool, default=False, help="Whether to take only newly queried samples in each active learning iterations, or the full training data")
-    parser.add_argument("--al_query_strategy", type=str, choices=["uncertainty_sampling", "margin_sampling", "entropy_sampling", "max_entropy", "bald", "random", "outlier", "cluster_outlier_combined"], default="uncertainty_sampling", help="Active learning query strategy")
+    parser.add_argument(
+        "--al_samples_per_iter", type=int, default=2000, help="No. of samples to query per active learning iteration"
+    )
+    parser.add_argument(
+        "--al_incr_onlynew",
+        type=bool,
+        default=False,
+        help="Whether to take only newly queried samples in each active learning iterations, or the full training data",
+    )
+    parser.add_argument(
+        "--al_query_strategy",
+        type=str,
+        choices=[
+            "uncertainty_sampling",
+            "margin_sampling",
+            "entropy_sampling",
+            "max_entropy",
+            "bald",
+            "random",
+            "outlier",
+            "cluster_outlier_combined",
+        ],
+        default="uncertainty_sampling",
+        help="Active learning query strategy",
+    )
 
     # For debugging / development purposes
-    parser.add_argument("--reduced_develop_train_size", type=bool, default=False, help="Whether to take only a very small set to train (allows for faster results during development)")
+    parser.add_argument(
+        "--reduced_develop_train_size",
+        type=bool,
+        default=False,
+        help="Whether to take only a very small set to train (allows for faster results during development)",
+    )
 
     parser.add_argument("--help", "-h", action="help")
     return parser
 
-def _log_skorch_history(history: skorch.history.History, al_iter: int, epoch_start: int, train_acc: float, train_size: int, wandb_logging: bool = True) -> None:
 
-    for epoch, train_loss, valid_loss, valid_acc, dur in history[:, ('epoch', 'train_loss', 'valid_loss', 'valid_acc', 'dur')]:
+def _log_skorch_history(
+    history: skorch.history.History,
+    al_iter: int,
+    epoch_start: int,
+    train_acc: float,
+    train_size: int,
+    wandb_logging: bool = True,
+) -> None:
+
+    for epoch, train_loss, valid_loss, valid_acc, dur in history[
+        :, ("epoch", "train_loss", "valid_loss", "valid_acc", "dur")
+    ]:
 
         metrics = {
-            'epoch': epoch_start + epoch, 
-            'train_loss': train_loss, 
-            'valid_loss': valid_loss, 
-            'valid_acc': valid_acc, 
-            'dur': dur,
-            'al_iter': al_iter, 
+            "epoch": epoch_start + epoch,
+            "train_loss": train_loss,
+            "valid_loss": valid_loss,
+            "valid_acc": valid_acc,
+            "dur": dur,
+            "al_iter": al_iter,
         }
-                
-        # add train_acc to last item of history
-        if epoch == len(history[:, 'train_loss']):
-            metrics['train_acc'] = train_acc
 
-        #print(metrics)
+        # add train_acc to last item of history
+        if epoch == len(history[:, "train_loss"]):
+            metrics["train_acc"] = train_acc
+
+        # print(metrics)
         if wandb_logging:
             wandb.log(metrics)
 
@@ -97,7 +143,7 @@ def main():
 
     Sample command:
     ```
-    python training/run_modAL_experiment.py --al_epochs_init=10 --al_epochs_incr=5 --al_n_iter=10 --al_samples_per_iter=100 --data_class=DroughtWatch --model_class=ResnetClassifier --batch_size=64 --n_train_images=1000 --n_validation_images=1000 --pretrained=True --wandb 
+    python training/run_modAL_experiment.py --al_epochs_init=10 --al_epochs_incr=5 --al_n_iter=10 --al_samples_per_iter=100 --data_class=DroughtWatch --model_class=ResnetClassifier --batch_size=64 --n_train_images=1000 --n_validation_images=1000 --pretrained=True --wandb
     ```
     """
 
@@ -127,7 +173,6 @@ def main():
 
     logger = pl.loggers.TensorBoardLogger("training/logs")
 
-
     # modAL specific experiment setup
     # -------------------------------
 
@@ -143,31 +188,34 @@ def main():
         query_strategy = _import_class(f"active_learning.sampling.{args.al_query_strategy}")
 
     # cpu vs. gpu: ignore --gpu args param, instead just set gpu based on availability
-    device = "cuda" if torch.cuda.is_available() else "cpu" 
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
-     # initialize train, validation and pool datasets
+    # initialize train, validation and pool datasets
     data.setup()
 
-    X_initial = np.moveaxis(data.data_train.data, 3, 1) # shape change: (i, channels, h, w) instead of (i, h, w, channels)
+    X_initial = np.moveaxis(
+        data.data_train.data, 3, 1
+    )  # shape change: (i, channels, h, w) instead of (i, h, w, channels)
     y_initial = data.data_train.targets
     if args.reduced_develop_train_size:
         print("NOTE: Reduced initial train set size for development activated")
         X_initial = X_initial[:100, :, :, :]
         y_initial = y_initial[:100]
 
-    X_val = np.moveaxis(data.data_val.data, 3, 1) # shape change
+    X_val = np.moveaxis(data.data_val.data, 3, 1)  # shape change
     y_val = data.data_val.targets
-    X_pool = np.moveaxis(data.data_unlabelled.data, 3, 1) # shape change
+    X_pool = np.moveaxis(data.data_unlabelled.data, 3, 1)  # shape change
     y_pool = data.data_unlabelled.targets
 
     # initialize skorch classifier
-    classifier = NeuralNetClassifier(model,
-                                     criterion=torch.nn.CrossEntropyLoss,
-                                     optimizer=torch.optim.Adam,
-                                     train_split=predefined_split(Dataset(X_val, y_val)),
-                                     verbose=1,
-                                     device=device,
-                                     )
+    classifier = NeuralNetClassifier(
+        model,
+        criterion=torch.nn.CrossEntropyLoss,
+        optimizer=torch.optim.Adam,
+        train_split=predefined_split(Dataset(X_val, y_val)),
+        verbose=1,
+        device=device,
+    )
 
     lit_model.summarize(mode="full")
 
@@ -175,36 +223,38 @@ def main():
     print("Initializing model with base training set")
     learner = ActiveLearner(
         estimator=classifier,
-        X_training=X_initial, 
-        y_training=y_initial, 
+        X_training=X_initial,
+        y_training=y_initial,
         epochs=args.al_epochs_init,
-        query_strategy=query_strategy
+        query_strategy=query_strategy,
     )
 
     _log_skorch_history(
-        history = learner.estimator.history, 
-        al_iter = 0, 
-        epoch_start = 0, 
-        train_acc = learner.score(learner.X_training, learner.y_training),
-        train_size = len(learner.y_training),
-        wandb_logging = args.wandb)
+        history=learner.estimator.history,
+        al_iter=0,
+        epoch_start=0,
+        train_acc=learner.score(learner.X_training, learner.y_training),
+        train_size=len(learner.y_training),
+        wandb_logging=args.wandb,
+    )
 
     # active learning loop
     for idx in range(args.al_n_iter):
 
-        print('Active learning query no. %d' % (idx + 1))
+        print("Active learning query no. %d" % (idx + 1))
         query_idx, _ = learner.query(X_pool, n_instances=args.al_samples_per_iter)
         learner.teach(
             X=X_pool[query_idx], y=y_pool[query_idx], only_new=args.al_incr_onlynew, epochs=args.al_epochs_incr
         )
 
         _log_skorch_history(
-            history = learner.estimator.history, 
-            al_iter = idx+1, 
-            epoch_start = args.al_epochs_init + idx*args.al_epochs_incr, 
-            train_acc = learner.score(learner.X_training, learner.y_training),
-            train_size = len(learner.y_training),
-            wandb_logging = args.wandb)
+            history=learner.estimator.history,
+            al_iter=idx + 1,
+            epoch_start=args.al_epochs_init + idx * args.al_epochs_incr,
+            train_acc=learner.score(learner.X_training, learner.y_training),
+            train_size=len(learner.y_training),
+            wandb_logging=args.wandb,
+        )
 
         # remove queried instances from pool
         X_pool = np.delete(X_pool, query_idx, axis=0)
