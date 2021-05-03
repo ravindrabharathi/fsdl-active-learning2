f2e23f7461995a9d94232a899164019e0ef6a0d2
Ravindra Bharathi <srbharathee@gmail.com>
2021-05-03 16:23:53 +0530

    try and log train size

--- active_learning/lit_models/base.py
+++ active_learning/lit_models/base.py
@@ -80,11 +80,16 @@ class BaseLitModel(pl.LightningModule):  # pylint: disable=too-many-ancestors
 
     def training_step(self, batch, batch_idx):  # pylint: disable=unused-argument
         x, y = batch
+        if (batch_idx==0):
+            self.train_size=0
+        batch_size=x.shape[0]
+        self.train_size +=batch_size
         logits = self(x)
         loss = self.loss_fn(logits, y)
         self.log("train_loss", loss,on_step=False, on_epoch=True,prog_bar=False)
         self.train_acc(logits, y)
         self.log("train_acc", self.train_acc, on_step=False, on_epoch=True, prog_bar=False)
+        self.log("train_size", self.train_size, on_step=False, on_epoch=True, prog_bar=False)
         return loss
 
     def validation_step(self, batch, batch_idx):  # pylint: disable=unused-argument
