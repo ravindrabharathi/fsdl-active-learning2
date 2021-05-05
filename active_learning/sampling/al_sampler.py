b3b153cf5bf0876d108ffcd31c3ae377246faded
stefan-jo <stefanjosef0@gmail.com>
2021-05-05 23:52:14 +0200

    added uncertainty monte carlo sampling methods

--- active_learning/sampling/al_sampler.py
+++ active_learning/sampling/al_sampler.py
@@ -10,7 +10,7 @@ def random(predictions, sample_size):
     if pool_size <= 2000:
         indices = [x for x in range(pool_size)]  # return complete pool size
     else:
-        indices = np.random.randint(pool_size, size=sample_size)
+        indices = np.random.choice(np.arange(pool_size), sample_size, replace=False)
 
     return indices
 
@@ -115,3 +115,183 @@ def max_entropy(probabilities: torch.Tensor, sample_size: int) -> np.array:
 
     idx = torch.argsort(-scores)[:sample_size].cpu().numpy()
     return idx
+
+
+def least_confidence_pt(predictions, sample_size: int) -> np.array:
+    """Uncertainty sampling technique from https://towardsdatascience.com/uncertainty-sampling-cheatsheet-ec57bc067c0b"""
+        
+    # convert np array to torch tensor
+    probabilities = torch.Tensor(predictions)
+
+    # get top probabilities
+    max_probs = torch.max(probabilities, dim=-1)[0]
+    # get number of classes
+    num_classes = probabilities[0].shape[0]
+    # calculate least confidence scores
+    scores = (num_classes * (1 - max_probs)) / (num_classes - 1)
+
+    # make sure sample size doesn't exceed scores
+    sample_size = np.min([len(scores), sample_size])
+
+    # pick k examples with highest uncertainty scores
+    _, idxs = torch.topk(scores, sample_size, largest=True)
+    idxs = idxs.detach().cpu().numpy()
+
+    return idxs
+
+
+def margin_pt(predictions, sample_size: int) -> np.array:
+    """Uncertainty sampling technique from https://towardsdatascience.com/uncertainty-sampling-cheatsheet-ec57bc067c0b"""
+        
+    # convert np array to torch tensor
+    probabilities = torch.Tensor(predictions)
+
+    # get two highest probabilities
+    max_probs, _ = torch.topk(probabilities, 2, largest=True, dim=1)
+    # calculate margins between two most confident predictions
+    margins = max_probs[:,0] - max_probs[:,1]
+    # calculate scores between 0 and 1
+    scores = 1 - margins
+
+    # make sure sample size doesn't exceed scores
+    sample_size = np.min([len(scores), sample_size])
+
+    # pick k examples with smallest margins
+    _, idxs = torch.topk(scores, sample_size, largest=True)
+    idxs = idxs.detach().cpu().numpy()
+
+    return idxs
+
+
+def ratio_pt(predictions, sample_size: int) -> np.array:
+    """Uncertainty sampling technique from https://towardsdatascience.com/uncertainty-sampling-cheatsheet-ec57bc067c0b"""
+
+    # convert np array to torch tensor
+    probabilities = torch.Tensor(predictions)
+
+    # get two highest probabilities
+    max_probs, _ = torch.topk(probabilities, 2, largest=True, dim=1)
+    # calculate ratio between two most confident predictions
+    scores = max_probs[:,1] / max_probs[:,0]
+
+    # make sure sample size doesn't exceed scores
+    sample_size = np.min([len(scores), sample_size])
+
+    # pick k examples with highest ratios
+    _, idxs = torch.topk(scores, sample_size, largest=True)
+    idxs = idxs.detach().cpu().numpy()
+
+    return idxs
+
+
+def entropy_pt(predictions, sample_size: int) -> np.array:
+    """Uncertainty sampling technique from https://towardsdatascience.com/uncertainty-sampling-cheatsheet-ec57bc067c0b"""
+
+    # convert np array to torch tensor
+    probabilities = torch.Tensor(predictions)
+
+    # calculate entropy
+    probslogs = probabilities * torch.log2(probabilities)
+    summed = probslogs.sum(dim=1)
+    scores = torch.subtract(torch.zeros_like(summed), summed) / np.log2(probslogs.size()[1])
+
+    # make sure sample size doesn't exceed scores
+    sample_size = np.min([len(scores), sample_size])
+
+    # pick k examples with highest entropy scores
+    _, idxs = torch.topk(scores, sample_size, largest=True)
+    idxs = idxs.detach().cpu().numpy()
+
+    return idxs
+
+
+def least_confidence_mc(probabilities: torch.Tensor, sample_size: int) -> np.array:
+    """Uncertainty sampling technique from https://towardsdatascience.com/uncertainty-sampling-cheatsheet-ec57bc067c0b
+    adjusted for Bayesian Active Learning"""
+
+    # calculate average probabilities from multiple inference runs
+    probabilities = torch.mean(probabilities, dim=2)
+        
+    # get top probabilities
+    max_probs = torch.max(probabilities, dim=-1)[0]
+    # get number of classes
+    num_classes = probabilities[0].shape[0]
+    # calculate least confidence scores
+    scores = (num_classes * (1 - max_probs)) / (num_classes - 1)
+
+    # make sure sample size doesn't exceed scores
+    sample_size = np.min([len(scores), sample_size])
+
+    # pick k examples with highest uncertainty scores
+    _, idxs = torch.topk(scores, sample_size, largest=True)
+    idxs = idxs.detach().cpu().numpy()
+
+    return idxs
+
+
+def margin_mc(probabilities: torch.Tensor, sample_size: int) -> np.array:
+    """Uncertainty sampling technique from https://towardsdatascience.com/uncertainty-sampling-cheatsheet-ec57bc067c0b
+    adjusted for Bayesian Active Learning"""
+
+    # calculate average probabilities from multiple inference runs
+    probabilities = torch.mean(probabilities, dim=2)
+
+    # get two highest probabilities
+    max_probs, _ = torch.topk(probabilities, 2, largest=True, dim=1)
+    # calculate margins between two most confident predictions
+    margins = max_probs[:,0] - max_probs[:,1]
+    # calculate scores between 0 and 1
+    scores = 1 - margins
+
+    # make sure sample size doesn't exceed scores
+    sample_size = np.min([len(scores), sample_size])
+
+    # pick k examples with smallest margins
+    _, idxs = torch.topk(scores, sample_size, largest=True)
+    idxs = idxs.detach().cpu().numpy()
+
+    return idxs
+
+
+def ratio_mc(probabilities: torch.Tensor, sample_size: int) -> np.array:
+    """Uncertainty sampling technique from https://towardsdatascience.com/uncertainty-sampling-cheatsheet-ec57bc067c0b
+    adjusted for Bayesian Active Learning"""
+
+    # calculate average probabilities from multiple inference runs
+    probabilities = torch.mean(probabilities, dim=2)
+
+    # get two highest probabilities
+    max_probs, _ = torch.topk(probabilities, 2, largest=True, dim=1)
+    # calculate ratio between two most confident predictions
+    scores = max_probs[:,1] / max_probs[:,0]
+
+    # make sure sample size doesn't exceed scores
+    sample_size = np.min([len(scores), sample_size])
+
+    # pick k examples with highest ratios
+    _, idxs = torch.topk(scores, sample_size, largest=True)
+    idxs = idxs.detach().cpu().numpy()
+
+    return idxs
+
+
+def entropy_mc(probabilities: torch.Tensor, sample_size: int) -> np.array:
+    """Uncertainty sampling technique from https://towardsdatascience.com/uncertainty-sampling-cheatsheet-ec57bc067c0b
+    adjusted for Bayesian Active Learning"""
+
+    # calculate average probabilities from multiple inference runs
+    probabilities = torch.mean(probabilities, dim=2)
+
+    # calculate entropy
+    probslogs = probabilities * torch.log2(probabilities)
+    summed = probslogs.sum(dim=1)
+    scores = torch.subtract(torch.zeros_like(summed), summed) / np.log2(probslogs.size()[1])
+
+    # make sure sample size doesn't exceed scores
+    sample_size = np.min([len(scores), sample_size])
+
+    # pick k examples with highest entropy scores
+    _, idxs = torch.topk(scores, sample_size, largest=True)
+    idxs = idxs.detach().cpu().numpy()
+
+    return idxs
